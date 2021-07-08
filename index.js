0443ffcb2276ad0d986762893430cd79efde482e
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-07-08 20:46:27 +0300

    homework

---
+++ index.js
@@ -0,0 +1,19 @@
+// const years = '21w00';
+// const regexp = /^(\d\W((19|20)(\d\d))|2100)$/g;
+// console.log(years.match(regexp));
+// console.log(years);
+
+// const times = '00:21';
+// const regexp = /^(([0-1][0-1]|20|21|22|23)\:([0-5][0-9]))$/g; 
+//or
+// const regexp = /^(([0-1][0-1]|[2][0-3])\:([0-5][0-9]))$/g; 
+// console.log(regexp.test(times));
+
+// const letters = 'rrttfdsaxccvbbbbb';
+// const regexp = /(.)(?=.*\1)/g;
+// console.log(letters.replace(regexp, ""));
+
+// const words = 'ccc bbb bbb aaa aaa';
+// const regexp = /\b(\w+)\s+\1/g;
+// console.log(words.replace(regexp, "$1"));
+
