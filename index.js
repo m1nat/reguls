d29ceb4a4c4905291da50f71c189b8bce9f60252
Vitaliy <mushpakov.vitaliy@gmail.com>
2021-07-09 19:03:04 +0300

    fixed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// const years = '21w00';
-// const regexp = /^(\d\W((19|20)(\d\d))|2100)$/g;
-// console.log(years.match(regexp));
-// console.log(years);
+const years = '2000';
+const regexp = /^(((19|20)[0-9][0-9])|2100)$/g;
+console.log(years.match(regexp));
+console.log(years);
 
 // const times = '00:21';
-// const regexp = /^(([0-1][0-1]|20|21|22|23)\:([0-5][0-9]))$/g; 
-//or
-// const regexp = /^(([0-1][0-1]|[2][0-3])\:([0-5][0-9]))$/g; 
+// const regexp = /^(([0-1][0-9]|20|21|22|23)\:([0-5][0-9]))$/g; 
+// or
+// const regexp = /^(([0-1][0-9]|[2][0-3])\:([0-5][0-9]))$/g; 
 // console.log(regexp.test(times));
 
 // const letters = 'rrttfdsaxccvbbbbb';
